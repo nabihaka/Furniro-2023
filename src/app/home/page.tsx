60bf185ac47a6f2c4bce34480c58ee75d81a70af
nabihaka <nakawirakrama@gmail.com>
2025-11-24 15:12:17 +0700

    Add new HomePage, modified home page, deleted HomePage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,19 +1,8 @@
 import React from "react";
-import { MainLayout } from "@/layout/MainLayout";
-import { HeroSection } from "@/components/home/Hero";
-import { BrowseRangeSection } from "@/components/home/BrowseRange";
-import { ProductsSection } from "@/components/home/Products";
-import { Inspiration } from "@/components/home/Inspiration";
-import { Portfolio } from "@/components/home/Portfolio";
+import { HomePage } from "@/components/pages/HomePage";
 
 export default function Home () {
   return (
-    <MainLayout>
-      <HeroSection />
-      <BrowseRangeSection />
-      <ProductsSection />
-      <Inspiration />
-      <Portfolio />
-    </MainLayout>
+    <HomePage />
   );
 };
