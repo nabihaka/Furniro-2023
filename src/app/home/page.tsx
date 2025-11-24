0d05fe044ad97d28cf0ca4e2c26228e1640330a4
nabihaka <nakawirakrama@gmail.com>
2025-11-24 14:45:04 +0700

    modified home page script

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,19 @@
 import React from "react";
-import { HomePage } from "@/pages/HomePage";
+import { MainLayout } from "@/layout/MainLayout";
+import { HeroSection } from "@/components/home/Hero";
+import { BrowseRangeSection } from "@/components/home/BrowseRange";
+import { ProductsSection } from "@/components/home/Products";
+import { Inspiration } from "@/components/home/Inspiration";
+import { Portfolio } from "@/components/home/Portfolio";
 
-export default function Home() {
-  return <HomePage />;
-}
+export default function Home () {
+  return (
+    <MainLayout>
+      <HeroSection />
+      <BrowseRangeSection />
+      <ProductsSection />
+      <Inspiration />
+      <Portfolio />
+    </MainLayout>
+  );
+};
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { MainLayout } from "@/layout/MainLayout";
-import { HeroSection } from "@/components/home/Hero";
-import { BrowseRangeSection } from "@/components/home/BrowseRange";
-import { ProductsSection } from "@/components/home/Products";
-import { Inspiration } from "@/components/home/Inspiration";
-import { Portfolio } from "@/components/home/Portfolio";
+// import React from "react";
+// import { MainLayout } from "@/layout/MainLayout";
+// import { HeroSection } from "@/components/home/Hero";
+// import { BrowseRangeSection } from "@/components/home/BrowseRange";
+// import { ProductsSection } from "@/components/home/Products";
+// import { Inspiration } from "@/components/home/Inspiration";
+// import { Portfolio } from "@/components/home/Portfolio";
 
-export const HomePage = () => {
-  return (
-    <MainLayout>
-      <HeroSection />
-      <BrowseRangeSection />
-      <ProductsSection />
-      <Inspiration />
-      <Portfolio />
-    </MainLayout>
-  );
-};
+// export const HomePage = () => {
+//   return (
+//     <MainLayout>
+//       <HeroSection />
+//       <BrowseRangeSection />
+//       <ProductsSection />
+//       <Inspiration />
+//       <Portfolio />
+//     </MainLayout>
+//   );
+// };
